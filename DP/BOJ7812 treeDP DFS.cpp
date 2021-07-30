1253d56d4a4fcdf0ebcf542dd0644c471e046c54
Garrrruii <hishally012@gmail.com>
2021-07-30 22:47:49 +0900

    21-07-30

--- DP/BOJ7812 treeDP DFS.cpp
+++ DP/BOJ7812 treeDP DFS.cpp
@@ -3,48 +3,24 @@
 using namespace std;
 
 bool vis[10000];
-int N, cnum[10000];
+int N;
+long long ANS = 0;
+vector<pair<int, int>> E[10000]; //{who,cost}
 
-typedef pair<int, int> pii;
-vector<pii> E[10000];
-
-pair<int,long long> dfs_tree(int cur) {
+long long dfs(int cur) {
 	vis[cur] = true;
 	int child_num = 1;
-	long long dist = 0;
-	pair<int, long long> ret;
 
-	for (pii e : E[cur]) {
-		int nxt = e.first;
-		if (vis[nxt]) continue;
+	for (pair<int, int> e : E[cur]) {
+		if (vis[e.first]) continue;
 
-		ret = dfs_tree(nxt);
-		cnum[nxt] = ret.first;
-		child_num += ret.first;
-		dist += (1LL * e.second * ret.first + ret.second);
-	}
-	
-	vis[cur] = false;
-	return { child_num,dist };
-}
-long long dfs_dist(int cur, long long ans) {
-	vis[cur] = true;
-//	cout << ans << " at " << cur << "\n";
-
-	long long rem = ans;
-	for (pii e : E[cur]) {
-		int nxt = e.first;
-		if (vis[nxt]) continue;
-	
-		int cnt = N - (cnum[nxt] << 1);
-		if (cnt > 0) continue;
-		long long tmp = dfs_dist(nxt, rem + 1LL * e.second * cnt);
-		ans = (ans < tmp) ? ans : tmp;
+		int cnt = dfs(e.first);
+		child_num += cnt;
+		ANS += 1LL * e.second * ((cnt << 1) < N ? cnt : N - cnt);
 	}
 
-//	cout << ans << " at " << cur << ": updated!\n";
 	vis[cur] = false;
-	return ans;
+	return child_num;
 }
 int main() {
 	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
@@ -58,9 +34,8 @@ int main() {
 			E[a].push_back({ b,c });
 			E[b].push_back({ a,c });
 		}
-		cout << dfs_dist(0, dfs_tree(0).second) << "\n";
+		ANS = 0; dfs(0); cout << ANS << "\n";
 
-		for (int i = 0; i < N; ++i) cnum[i] = 0;
 		for (int i = 0; i < N; ++i) E[i].clear();
 	}
 }
